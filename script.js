95487a298a1375c29e4ad0fb65e773e8fd9d8581
Andy <andyaoux@hotmail.com>
2017-04-28 23:35:57 -0400

    change

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	
+	//Type.js 
+	$(function(){
+		$(".open").typed({
+			strings: ["Hey, its Andy"],
+			typeSpeed: 20,
+			showCursor: false,
+		});
+		$(".prob").typed({
+			strings: ["Im Probably,"],
+			typeSpeed: 10,
+			showCursor:false,
+		})
+		$(".about").typed({
+			strings: ["Studying Computer Science @Carleton","Learning Ruby","Waiting For Yeezys","Skateboarding",
+			"Watching Videos on Youtube","Drinking Coffee","Programming","Waiting for the bus","Making Memes","Listening to Porter Robinson"
+			,"Watching Anime","Running","Travelling","Making a Goof"],
+			typeSpeed: 10,
+            backDelay: 1250,
+            showCursor: false,
+            loop: true
+		});
+
+	});
+});
